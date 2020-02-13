40926b263941a1a864aabee77aeef93775f9bcb2
Marcelo Goto <marcelo.goto@nvstec.com>
2020-02-13 11:35:39 -0300

    Added events getter method

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,7 @@ export class HomePage {
   user: any;
   events: any;
   isAuthenticated: boolean = false;
+  token: any;
 
   constructor(
     private msAdal: MSAdal,
@@ -33,6 +34,7 @@ export class HomePage {
 
     authContext.acquireTokenAsync('https://graph.microsoft.com', '03d4b82a-06df-4c21-99bd-ee5fec338c1f', 'com.nvstec.app.nvsmeeting://home','','')
     .then((authResponse: AuthenticationResult) => {
+      this.token = authResponse.accessToken;
       this.http.get("https://graph.microsoft.com/v1.0/me",{
         headers: new HttpHeaders({"Authorization": "Bearer "+ authResponse.accessToken})
       }).subscribe(res => {
@@ -41,5 +43,21 @@ export class HomePage {
       })
     })
     .catch((e: any) => console.log('Authentication failed', e));
+  }
+
+  async getEvents(){
+
+    const loading = await this.loadingCtrl.create({
+      message: 'Buscando eventos'
+    });
+
+    await loading.present();
+    
+    this.http.get("https://graph.microsoft.com/v1.0/me/events",{
+        headers: new HttpHeaders({"Authorization": "Bearer "+ this.token})
+      }).subscribe(res => {
+        loading.dismiss();
+        console.log("events",res);
+      })
   }
 }
