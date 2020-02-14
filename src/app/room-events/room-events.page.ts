db2b35315c28121f0b80f571220e2e6559844a5c
Marcelo Goto <marcelo.goto@nvstec.com>
2020-02-14 15:38:05 -0300

    Implemented logic to check if there is a meeting now

--- src/app/room-events/room-events.page.ts
+++ src/app/room-events/room-events.page.ts
@@ -16,6 +16,7 @@ export class RoomEventsPage implements OnInit {
   clock: any = new Date();
   timer: any;
   todayEventsArray = [];
+  currentMeeting: any = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,6 +31,7 @@ export class RoomEventsPage implements OnInit {
         this.roomName = this.router.getCurrentNavigation().extras.state.roomName;
         console.log("obj",this.obj);
         this.createTodayEventsArray(this.obj);
+        this.checkMeetingNow(this.obj);
       }
     });
   }
@@ -48,7 +50,6 @@ export class RoomEventsPage implements OnInit {
 
   createTodayEventsArray(array:any){
     let now = new Date();
-    console.log("now",now);
     array.forEach(element => {
       let dateStartParsed = new Date(element.start.dateTime);
       let dateEndParsed = new Date(element.end.dateTime);
@@ -130,6 +131,57 @@ export class RoomEventsPage implements OnInit {
             formattedEndMinutes: formattedEndMinute
           }
           this.todayEventsArray.push(temp);
+        }
+      }
+    });
+    this.todayEventsArray.reverse();
+  }
+
+  checkMeetingNow(array:any){
+    let now = new Date();
+    array.forEach(element => {
+      let dateStartParsed = new Date(element.start.dateTime);
+      let dateEndParsed = new Date(element.end.dateTime);
+      if(dateStartParsed.getDate() == now.getDate() && dateStartParsed.getMonth() == now.getMonth() && dateStartParsed.getFullYear() == now.getFullYear()){
+        if(dateStartParsed.getHours()-3 == now.getHours()+1){
+          if(dateStartParsed.getMinutes() <= now.getMinutes() && dateEndParsed.getMinutes() >= now.getMinutes()){
+            let formattedStartHour;
+            let formattedStartMinute;
+            let formattedEndHour;
+            let formattedEndMinute;
+            if(dateStartParsed.getHours()-3 == 0){
+              formattedStartHour = "00";
+            }
+            else{
+              formattedStartHour = dateStartParsed.getHours()-3;
+            }
+            if(dateEndParsed.getHours()-3 == 0){
+              formattedEndHour = "00";
+            }
+            else{
+              formattedEndHour = dateEndParsed.getHours()-3;
+            }
+            if(dateStartParsed.getMinutes() == 0){
+              formattedStartMinute = "00";
+            }
+            else{
+              formattedStartMinute = dateStartParsed.getMinutes();
+            }
+            if(dateEndParsed.getMinutes() == 0){
+              formattedEndMinute = "00";
+            }
+            else{
+              formattedEndMinute = dateEndParsed.getMinutes()
+            }
+            let obj = {
+              subject: element.subject,
+              formattedStartHour: formattedStartHour,
+              formattedStartMinutes: formattedStartMinute,
+              formattedEndHour: formattedEndHour,
+              formattedEndMinutes: formattedEndMinute
+            }
+            this.currentMeeting = obj;
+          }
         }
       }
     });
