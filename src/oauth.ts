1c051f8a4b8a9eb33d4d1504567e9874940abbb3
Jonathan <jonathan@umbara.co>
2024-11-06 14:52:28 -0300

    chore: add environment file

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,5 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const OAuthSettings = {
-    appId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    appId: process.env.APP_ID,
     scopes: [
         "user.read",
         "calendars.read"
